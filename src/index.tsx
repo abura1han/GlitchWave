11a0d935004cc0b2e46e92c18f1cc6a67391c697
abura1han <xbura1han@gmail.com>
2022-04-02 17:12:08 +0600

    Fix react 18 create-root typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
