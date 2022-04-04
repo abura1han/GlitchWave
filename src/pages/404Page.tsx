03a05e75d0660085512156db8c487d886a938898
abura1han <xbura1han@gmail.com>
2022-04-04 17:44:14 +0600

    Add 404 error content

--- src/pages/404Page.tsx
+++ src/pages/404Page.tsx
@@ -1,12 +1,25 @@
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import ContentBox from "../components/contents/ContentBox";
 import PageWrapper from "../components/PageWrapper";
 
 // 404 page not found error page
 const NotFoundPage = () => {
   return (
     <PageWrapper>
-      <div className="text-xl text-gray-300">
-        <div>404 Page not found</div>
-      </div>
+      <ContentBox name="404 Page not found">
+        <div className="w-full border-b border-b-glitch-box"></div>
+        <div className="px-5 py-5">
+          <h2 className="text-lg text-rose-500">Error</h2>
+          <h1 className="text-6xl text-white font-bold">404</h1>
+          <h2 className="text-lg text-glitch-orange">Page not found</h2>
+          <Link to={"/"}>
+            <Button className="hover:bg-glitch-orange mt-5">
+              <span>Back to home</span>
+            </Button>
+          </Link>
+        </div>
+      </ContentBox>
     </PageWrapper>
   );
 };
